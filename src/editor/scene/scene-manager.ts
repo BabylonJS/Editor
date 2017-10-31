06ef11ccf4f2a1a533ae56177dc6f77c646e93af
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-10-31 22:04:40 +0100

    Now pick easily meshes from base scene + loaded scenes

--- src/editor/scene/scene-manager.ts
+++ src/editor/scene/scene-manager.ts
@@ -1,5 +1,29 @@
-import { ActionManager } from 'babylonjs';
+import { Scene, ActionManager } from 'babylonjs';
+import { IStringDictionary } from '../typings/typings';
 
 export default class SceneManager {
+    // Public members
+    public static ActionManagers: IStringDictionary<ActionManager> = { };
 
+    /**
+     * Clears the scene manager
+     */
+    public static Clear (): void {
+        this.ActionManagers = { };
+    }
+
+    /**
+     * Toggles all interaction events to disable but keep
+     * references like Action Manager references etc.
+     * @param scene the scene to toggle
+     */
+    public static Toggle (scene: Scene): void {
+        scene.meshes.forEach(m => {
+            const savedActionManager = this.ActionManagers[m.id] || null;
+            const currentActionManager = m.actionManager;
+
+            this.ActionManagers[m.id] = currentActionManager;
+            m.actionManager = savedActionManager;
+        });
+    }
 }
