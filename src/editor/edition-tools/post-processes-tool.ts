c7b52c25671cef314ace60cbc42fc12e82a5e5b2
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-11-10 18:29:29 +0100

    Adding SSAO in PostProcess edition tool

--- src/editor/edition-tools/post-processes-tool.ts
+++ src/editor/edition-tools/post-processes-tool.ts
@@ -1,7 +1,7 @@
 import {
     Scene, Texture,
     SpotLight, DirectionalLight,
-    StandardRenderingPipeline
+    StandardRenderingPipeline, SSAORenderingPipeline
 } from 'babylonjs';
 
 import AbstractEditionTool from './edition-tool';
@@ -17,6 +17,7 @@ export default class PostProcessesTool extends AbstractEditionTool<Scene> {
 
     // Private members
     private _standardEnabled: boolean = false;
+    private _ssaoEnabled: boolean = false;
 
 	/**
 	* Returns if the object is supported
@@ -58,41 +59,41 @@ export default class PostProcessesTool extends AbstractEditionTool<Scene> {
             const bloom = standardPipeline.addFolder('Bloom');
             bloom.open();
             bloom.add(SceneManager.StandardRenderingPipeline, 'BloomEnabled').name('Bloom Enabled');
-            bloom.add(SceneManager.StandardRenderingPipeline, "exposure").min(0).max(10).step(0.01).name("Exposure");
-            bloom.add(SceneManager.StandardRenderingPipeline, "brightThreshold").min(0).max(10).step(0.01).name("Bright Threshold");
-            bloom.add(SceneManager.StandardRenderingPipeline, "blurWidth").min(0).max(512).step(0.01).name("Blur Width");
-            bloom.add(SceneManager.StandardRenderingPipeline, "horizontalBlur").name("Horizontal Blur");
+            bloom.add(SceneManager.StandardRenderingPipeline, 'exposure').min(0).max(10).step(0.01).name('Exposure');
+            bloom.add(SceneManager.StandardRenderingPipeline, 'brightThreshold').min(0).max(10).step(0.01).name('Bright Threshold');
+            bloom.add(SceneManager.StandardRenderingPipeline, 'blurWidth').min(0).max(512).step(0.01).name('Blur Width');
+            bloom.add(SceneManager.StandardRenderingPipeline, 'horizontalBlur').name('Horizontal Blur');
 
             const motionBlur = standardPipeline.addFolder('Motion Blur');
             motionBlur.open();
             motionBlur.add(SceneManager.StandardRenderingPipeline, 'MotionBlurEnabled').name('Motion Blur Enabled');
-            motionBlur.add(SceneManager.StandardRenderingPipeline, "motionBlurSamples").min(1).max(64).step(1).name("Samples Count");
-            motionBlur.add(SceneManager.StandardRenderingPipeline, "motionStrength").min(0).step(0.01).name("Strength");
+            motionBlur.add(SceneManager.StandardRenderingPipeline, 'motionBlurSamples').min(1).max(64).step(1).name('Samples Count');
+            motionBlur.add(SceneManager.StandardRenderingPipeline, 'motionStrength').min(0).step(0.01).name('Strength');
 
             const lensFlare = standardPipeline.addFolder('Lens Flare');
             lensFlare.open();
-            lensFlare.add(SceneManager.StandardRenderingPipeline, "LensFlareEnabled").name("Lens Flare Enabled");
-            lensFlare.add(SceneManager.StandardRenderingPipeline, "lensFlareStrength").min(0).max(100).step(0.01).name("Strength");
-            lensFlare.add(SceneManager.StandardRenderingPipeline, "lensFlareHaloWidth").min(0).max(2).step(0.01).name("Halo Width");
-            lensFlare.add(SceneManager.StandardRenderingPipeline, "lensFlareGhostDispersal").min(0).max(10).step(0.1).name("Ghost Dispersal");
-            lensFlare.add(SceneManager.StandardRenderingPipeline, "lensFlareDistortionStrength").min(0).max(500).step(0.1).name("Distortion Strength");
+            lensFlare.add(SceneManager.StandardRenderingPipeline, 'LensFlareEnabled').name('Lens Flare Enabled');
+            lensFlare.add(SceneManager.StandardRenderingPipeline, 'lensFlareStrength').min(0).max(100).step(0.01).name('Strength');
+            lensFlare.add(SceneManager.StandardRenderingPipeline, 'lensFlareHaloWidth').min(0).max(2).step(0.01).name('Halo Width');
+            lensFlare.add(SceneManager.StandardRenderingPipeline, 'lensFlareGhostDispersal').min(0).max(10).step(0.1).name('Ghost Dispersal');
+            lensFlare.add(SceneManager.StandardRenderingPipeline, 'lensFlareDistortionStrength').min(0).max(500).step(0.1).name('Distortion Strength');
 
             const dof = standardPipeline.addFolder('Depth-Of-Field');
             dof.open();
-            dof.add(SceneManager.StandardRenderingPipeline, "DepthOfFieldEnabled").name("Depth-Of-Field Enabled");
-            dof.add(SceneManager.StandardRenderingPipeline, "depthOfFieldDistance").min(0).max(1).step(0.01).name("DOF Distance");
-            dof.add(SceneManager.StandardRenderingPipeline, "depthOfFieldBlurWidth").min(0).max(512).name("Blur Width");
+            dof.add(SceneManager.StandardRenderingPipeline, 'DepthOfFieldEnabled').name('Depth-Of-Field Enabled');
+            dof.add(SceneManager.StandardRenderingPipeline, 'depthOfFieldDistance').min(0).max(1).step(0.01).name('DOF Distance');
+            dof.add(SceneManager.StandardRenderingPipeline, 'depthOfFieldBlurWidth').min(0).max(512).name('Blur Width');
 
             const hdr = standardPipeline.addFolder('HDR');
             hdr.open();
-            hdr.add(SceneManager.StandardRenderingPipeline, "HDREnabled").name("HDR Enabled");
-            hdr.add(SceneManager.StandardRenderingPipeline, "hdrMinimumLuminance").min(0).max(2).name("Minimum Luminance");
-            hdr.add(SceneManager.StandardRenderingPipeline, "hdrDecreaseRate").min(0).max(2).name("Decrease Rate");
-            hdr.add(SceneManager.StandardRenderingPipeline, "hdrIncreaseRate").min(0).max(2).name("Increase Rate");
+            hdr.add(SceneManager.StandardRenderingPipeline, 'HDREnabled').name('HDR Enabled');
+            hdr.add(SceneManager.StandardRenderingPipeline, 'hdrMinimumLuminance').min(0).max(2).name('Minimum Luminance');
+            hdr.add(SceneManager.StandardRenderingPipeline, 'hdrDecreaseRate').min(0).max(2).name('Decrease Rate');
+            hdr.add(SceneManager.StandardRenderingPipeline, 'hdrIncreaseRate').min(0).max(2).name('Increase Rate');
 
             const vls = standardPipeline.addFolder('Volumetric Lights');
             vls.open();
-            vls.add(SceneManager.StandardRenderingPipeline, "VLSEnabled").name("Volumetric Lights Enabled").onChange(r => {
+            vls.add(SceneManager.StandardRenderingPipeline, 'VLSEnabled').name('Volumetric Lights Enabled').onChange(r => {
                 const picker = new Picker('Select Light Emitter');
                 picker.addItems(scene.lights.map(l => (l instanceof SpotLight || l instanceof DirectionalLight) && l));
                 picker.open((items) => {
@@ -103,11 +104,30 @@ export default class PostProcessesTool extends AbstractEditionTool<Scene> {
                 });
             });
             if (SceneManager.StandardRenderingPipeline.VLSEnabled) {
-                vls.add(SceneManager.StandardRenderingPipeline, "volumetricLightCoefficient").min(0).max(1).step(0.01).name("Scattering Coefficient");
-                vls.add(SceneManager.StandardRenderingPipeline, "volumetricLightPower").min(0).max(10).step(0.01).name("Scattering Power");
-                vls.add(SceneManager.StandardRenderingPipeline, "volumetricLightBlurScale").min(0).max(64).step(1).name("Blur scale");
-                vls.add(SceneManager.StandardRenderingPipeline, "volumetricLightStepsCount").min(0).max(100).step(1).name("Steps count");
+                vls.add(SceneManager.StandardRenderingPipeline, 'volumetricLightCoefficient').min(0).max(1).step(0.01).name('Scattering Coefficient');
+                vls.add(SceneManager.StandardRenderingPipeline, 'volumetricLightPower').min(0).max(10).step(0.01).name('Scattering Power');
+                vls.add(SceneManager.StandardRenderingPipeline, 'volumetricLightBlurScale').min(0).max(64).step(1).name('Blur scale');
+                vls.add(SceneManager.StandardRenderingPipeline, 'volumetricLightStepsCount').min(0).max(100).step(1).name('Steps count');
             }
         }
+
+        // SSAO
+        const ssao = this.tool.addFolder('SSAO');
+        ssao.open();
+
+        this._ssaoEnabled = SceneManager.SSAORenderingPipeline !== null;
+        ssao.add(this, '_ssaoEnabled').name('Enable').onChange(async r => {
+            const pipeline = new SSAORenderingPipeline('SSAO', scene, { ssaoRatio: 0.5, combineRatio: 1.0 }, scene.cameras);
+            pipeline.fallOff = 0.000001;
+            pipeline.area = 1.0;
+            pipeline.radius = 0.00023;
+            pipeline.totalStrength = 1;
+            pipeline.base = 0.5;
+
+            scene.postProcessRenderPipelineManager.disableEffectInPipeline('SSAO', pipeline.SSAOCombineRenderEffect, this.editor.camera);
+
+            SceneManager.SSAORenderingPipeline = pipeline;
+            this.update(scene);
+        });
     }
 }
