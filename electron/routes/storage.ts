cefde5008ae6845f01a3bd6e28ff7a791e49c95e
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-11-15 22:30:18 +0100

    Working on electron web server: storage routes

--- electron/routes/storage.ts
+++ electron/routes/storage.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
 import * as Koa from 'koa';
 import * as KoaRouter from 'koa-router';
 
@@ -5,6 +8,9 @@ export default class StorageRouter {
     // Public members
     public router: KoaRouter;
     public application: Koa;
+
+    // Protected members
+    protected path: string = process.cwd();
 
     /**
      * Constructor
@@ -16,6 +22,9 @@ export default class StorageRouter {
 
         // Create routes
         this.getFiles();
+        this.writeFile();
+        this.createFolder();
+        
         this.application.use(this.router.routes());
     }
 
@@ -23,10 +32,48 @@ export default class StorageRouter {
      * Returns the files
      */
     protected getFiles (): void {
-        this.router.get('/files', async (next) => {
-            next.body = {
-                // TODO: add files
+        this.router.get('/files', async (ctx, next) => {
+            this.path = path.resolve(this.path + '/' + ((ctx.query && ctx.query.path) ? ctx.query.path : ''));
+            const entries = await fs.readdir(this.path);
+            const files: { name: string, folder: string }[] = [{ name: '..', folder: '..' }];
+
+            for (const e of entries) {
+                const stat = await fs.stat(path.resolve(this.path, e));
+                files.push({ name: e, folder: stat.isDirectory() ? e : null });
             }
+
+            ctx.body = {
+                value: files
+            };
+        });
+    }
+
+    /**
+     * Writes the given file
+     */
+    protected writeFile (): void {
+        this.router.put('/files:/write', async (ctx, next) => {
+            const chunks = [];
+            ctx.req.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+            ctx.req.on('end', async () => {
+                const buffer = Buffer.concat(chunks);
+
+                // Write file
+                const filename = path.resolve(this.path, (ctx.query && ctx.query.name) ? ctx.query.name : '');
+                await fs.writeFile(filename, buffer);
+            });
+        });
+    }
+
+    /**
+     * Creates a folder
+     */
+    protected createFolder (): void {
+        this.router.post('/files:/folder', async (ctx, next) => {
+            const folder = (ctx.body && ctx.body.name) ? ctx.body.name : '';
+            await fs.mkdir(folder);
         });
     }
 }
---
+++ src/editor/storage/electron-storage.ts
@@ -0,0 +1,49 @@
+import Editor from '../editor';
+import Tools from '../tools/tools';
+import Request from '../tools/request';
+
+import Storage, { CreateFiles, GetFiles } from './storage';
+
+export default class ElectronStorage extends Storage {
+    /**
+     * Constructor
+     * @param editor: the editor reference
+     */
+    constructor (editor: Editor) {
+        super(editor);
+    }
+
+    /**
+     * Creates the given folders
+     * @param folder the parent folder
+     * @param names the folders names
+     */
+    public async createFolders (folder: any, names: string[]): Promise<void> {
+
+    }
+    
+    /**
+     * Creates the given files
+     * @param folder the parent folder
+     * @param files the files to write
+     */
+    public async createFiles (folder: any, files: CreateFiles[]): Promise<void> {
+        for (const f of files) {
+            await Request.Put('http://localhost:1338/files:/write?name=' + f.name, f.data);
+        }
+    }
+
+    /**
+     * Returns the files available in the given folder
+     * @param folder the parent folder
+     */
+    public async getFiles (folder: string): Promise<GetFiles[]> {
+        const files = await Request.Get<any>('http://localhost:1338/files' + (folder ? '?path=' + folder : ''));
+
+        const result: GetFiles[] = [];
+        files.value.forEach(v => {
+            result.push({ name: v.name, folder: v.folder ? v.folder : null });
+        });
+        return result;
+    }
+}
