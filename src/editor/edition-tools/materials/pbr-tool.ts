d6292691dbab9093d6eb7cb58339ff23e827f2df
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-11-21 19:42:52 +0100

    Finished PBR Tool

--- src/editor/edition-tools/materials/pbr-tool.ts
+++ src/editor/edition-tools/materials/pbr-tool.ts
@@ -50,6 +50,7 @@ export default class PBRTool extends MaterialTool<PBRMaterial> {
         reflection.open();
         this.tool.addTexture(reflection, this.editor.core.scene, 'reflectionTexture', this.object, true, true).name('Reflection Texture');
         this.tool.addColor(reflection, 'Color', this.object.reflectionColor).open();
+        reflection.add(this.object, 'environmentIntensity').step(0.01).name('Environment Intensity');
 
         // Microsurface
         const micro = this.tool.addFolder('Micro Surface');
@@ -58,6 +59,42 @@ export default class PBRTool extends MaterialTool<PBRMaterial> {
         micro.add(this.object, 'microSurface').min(0).max(1).name('Micro Surface');
         micro.add(this.object, 'useAutoMicroSurfaceFromReflectivityMap').name('Use Auto Micro Surface From Reflectivity Map');
         micro.add(this.object, 'useMicroSurfaceFromReflectivityMapAlpha').name('Use Micro Surface From Reflectivity Map Alpha');
+
+        // Metallic
+        const metallic = this.tool.addFolder('Metallic');
+        metallic.open();
+        metallic.add(this.object, 'useMetallnessFromMetallicTextureBlue').name('Metallness From Metallic Texture Blue');
+        metallic.add(this.object, 'useRoughnessFromMetallicTextureAlpha').name('Use Roughness From Metallic Texture Alpha');
+        metallic.add(this.object, 'useRoughnessFromMetallicTextureGreen').name('Use Roughness From Metallic Texture Green');
+        this.tool.addTexture(metallic, this.editor.core.scene, 'metallicTexture', this.object, false).name('Metallic Texture');
+
+        // Emissive
+        const emissive = this.tool.addFolder('Emissive');
+        emissive.open();
+        this.tool.addColor(emissive, 'Emissive', this.object.emissiveColor).open();
+        emissive.add(this.object, 'emissiveIntensity').step(0.01).name('Emissive Intensity');
+        this.tool.addTexture(emissive, this.editor.core.scene, 'emissiveTexture', this.object).name('Emissive Texture');
+
+        // Ambient
+        const ambient = this.tool.addFolder('Ambient');
+        ambient.open();
+        this.tool.addColor(ambient, 'Ambient', this.object.ambientColor).open();
+        this.tool.addTexture(ambient, this.editor.core.scene, 'ambientTexture', this.object).name('Ambient Texture');
+        ambient.add(this.object, 'ambientTextureStrength').step(0.01).name('Ambient Texture Strength');
+
+        // Light map
+        const lightmap = this.tool.addFolder('Lightmap');
+        lightmap.open();
+        lightmap.add(this.object, 'useLightmapAsShadowmap').name('Use Lightmap As Shadowmap');
+        this.tool.addTexture(lightmap, this.editor.core.scene, 'lightmapTexture', this.object).name('Lightmap Texture');
+
+        // Refraction
+        const refraction = this.tool.addFolder('Refraction');
+        refraction.open();
+        refraction.add(this.object, 'indexOfRefraction').step(0.01).name('Index of Refraction');
+        refraction.add(this.object, 'invertRefractionY').name('Invert Y');
+        refraction.add(this.object, 'linkRefractionWithTransparency').name('Link Refraction With Transparency');
+        this.tool.addTexture(refraction, this.editor.core.scene, 'refractionTexture', this.object, true).name('Refraction Texture');
 
         // Options
         super.addOptions();
