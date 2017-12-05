42dc7ea5fca8b4b47adca071a6ec23dae9c928f2
Julien Moreau-Mathis <moreau-mathis@intechinfo.fr>
2017-12-05 20:40:29 +0100

    Now bundle editor extenions

---
+++ src/extensions/index.ts
@@ -0,0 +1,13 @@
+import Extensions from './extensions';
+import Extension from './extension';
+
+import CodeExtension from './behavior/code';
+import SpectorDebug from './spector/spector-debug';
+
+export {
+    Extensions,
+    Extension,
+
+    CodeExtension,
+    SpectorDebug
+}
