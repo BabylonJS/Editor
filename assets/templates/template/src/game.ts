c92cc7e349f88e3ede054e4b94d16977ab71b897
the-simian <harlinjesse@gmail.com>
2019-06-10 15:26:42 -0500

    revert game, correc timports

--- assets/templates/template/src/game.ts
+++ assets/templates/template/src/game.ts
@@ -15,45 +15,58 @@ import {
 import { Extensions } from "babylonjs-editor";
 
 export default class Game {
+  // Public members
   public engine: Engine;
   public canvas: HTMLCanvasElement = <HTMLCanvasElement>(
     document.getElementById("renderCanvas")
   );
 
   public scene: Scene = null;
+
+  /**
+   * Constructor
+   */
   constructor() {
-    this.engine = new Engine(this.canvas, true, {});
+    // Create engine
+    this.engine = new Engine(this.canvas, true, {
+      // Options
+    });
+
+    // Events
     window.addEventListener("resize", () => this.engine.resize());
   }
 
+  /**
+   * Runs the game
+   */
   public run(): void {
-    const rainyDay = `./scenes/Rainy-Day/`;
-    //const spaceScene = `./scenes/Space-Scene/`;
-    let currentScene = rainyDay;
-
+    // Load Scene
     SceneLoader.Load(
-      `${currentScene}`,
-      "scene.babylon",
+      "./scene/",
+      "scene.{{scene_format}}",
       this.engine,
       (scene: Scene) => {
         this.scene = scene;
-        console.log(this.scene);
+
+        // No camera?
         if (!this.scene.activeCamera) {
           this.scene.createDefaultCamera(false, true, true);
         }
 
+        // Attach camera
         this.scene.activeCamera.attachControl(this.canvas, true);
 
-        Tools.LoadFile(
-          `${currentScene}/project.editorproject`,
-          (data: string) => {
-            Extensions.RoolUrl = currentScene;
-            Extensions.ApplyExtensions(this.scene, JSON.parse(data));
-            this.engine.runRenderLoop(() => {
-              this.scene.render();
-            });
-          }
-        );
+        // Load extensions
+        Tools.LoadFile("./scene/project.editorproject", (data: string) => {
+          // Apply extensions (such as custom code, custom materials etc.)
+          Extensions.RoolUrl = "./scene/";
+          Extensions.ApplyExtensions(this.scene, JSON.parse(data));
+
+          // Run render loop
+          this.engine.runRenderLoop(() => {
+            this.scene.render();
+          });
+        });
       }
     );
   }
