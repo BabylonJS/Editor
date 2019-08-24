88f2e65e99a0e4e582701e57977a1145ab5f753a
Julien MOREAU-MATHIS <moreau-mathis@intechinfo.fr>
2019-08-24 15:19:04 +0200

    Added install and updatePackages scripts.

---
+++ script/updatePackages.js
@@ -0,0 +1,32 @@
+const yargs = require('yargs');
+const fs = require('fs');
+
+const args = yargs.argv;
+
+const mainPackageJson = JSON.parse(fs.readFileSync('./package.json'));
+const version = args._[0] || mainPackageJson.version;
+
+const updatePackageJson = function (path) {
+    const json = JSON.parse(fs.readFileSync(path));
+
+    // Update version
+    json.version = version;
+    // Update packages versions
+    for (const k in json.dependencies) {
+        if (!mainPackageJson.dependencies[k])
+            continue;
+
+        json.dependencies[k] = mainPackageJson.dependencies[k];
+    }
+    
+    // Write file
+    fs.writeFileSync(path, JSON.stringify(json, null, '  '), { encoding: 'utf-8' });
+
+    // Log
+    console.log('Updated: ', path);
+}
+
+updatePackageJson('./photoshop-extension/package.json');
+updatePackageJson('./vscode-extension/package.json');
+updatePackageJson('./assets/templates/template/package.json');
+updatePackageJson('./package.json');
