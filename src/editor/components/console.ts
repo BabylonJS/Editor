3cfab899c045d015277c73381b21985bcea0e3e2
Julien MOREAU-MATHIS <moreau-mathis@intechinfo.fr>
2019-10-19 20:37:20 +0200

    Added filters to editor console.

--- src/editor/components/console.ts
+++ src/editor/components/console.ts
@@ -28,6 +28,12 @@ interface LogMessage {
     level: ConsoleLevel;
 }
 
+interface Filters {
+    [ConsoleLevel.INFO]: boolean;
+    [ConsoleLevel.WARN]: boolean;
+    [ConsoleLevel.ERROR]: boolean;
+}
+
 export default class EditorConsole {
     /**
      * The layout used to draw toolbar etc.
@@ -45,6 +51,12 @@ export default class EditorConsole {
     private _messages: LogMessage[] = [];
     private _autoScroll: boolean = true;
     private _addingLog: boolean = false;
+
+    private _filters: Filters = {
+        [ConsoleLevel.INFO]: true,
+        [ConsoleLevel.WARN]: true,
+        [ConsoleLevel.ERROR]: true
+    };
 
     /**
      * Defines the maximum number of logs available in the console.
@@ -67,38 +79,14 @@ export default class EditorConsole {
         // Toolbar
         this.toolbar = new Toolbar('CONSOLE-TOOLBAR');
         this.toolbar.items = [
-            { type: 'button', id: 'clear', text: 'clear', img: 'icon-recycle' }
+            { type: 'button', id: 'clear', text: 'clear', img: 'icon-recycle' },
+            { type: 'break' },
+            { type: 'button', id: 'informations', text: 'informations', checked: true },
+            { type: 'button', id: 'warnings', text: 'warnings', checked: true },
+            { type: 'button', id: 'errors', text: 'errors', checked: true }
         ];
         this.toolbar.onClick = id => this._onToolbarClick(id);
         this.toolbar.build('CONSOLE-TOOLBAR');
-
-        // Set language
-        window['monaco'].languages.register({ id: 'consoleLanguage' });
-        window['monaco'].languages.setMonarchTokensProvider('consoleLanguage', {
-            tokenizer: {
-                root: [
-                    [/\[error.*/, "log-error"],
-                    [/\[warn.*/, "log-notice"],
-                    [/\[info.*/, "log-info"],
-                    [/\[[a-zA-Z 0-9:]+\]/, "log-date"],
-                ]
-            }
-        });
-
-        window['monaco'].editor.defineTheme('consoleTheme', {
-            base: ThemeSwitcher.ThemeName === 'Dark' ? 'vs-dark' : 'vs',
-            inherit: false,
-            rules: [
-                { token: 'log-info', foreground: '808080' },
-                { token: 'log-error', foreground: 'ff0000', fontStyle: 'bold' },
-                { token: 'log-warn', foreground: 'FFA500' },
-                { token: 'log-date', foreground: '008800' },
-            ]
-        });
-
-        window['monaco'].languages.registerCompletionItemProvider('consoleLanguage', {
-            provideCompletionItems: () => []
-        });
 
         // Code Editor
         this._createEditor();
@@ -119,24 +107,33 @@ export default class EditorConsole {
         const date = new Date(Date.now());
         const dateStr = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
 
+        let log: LogMessage;
         switch (level) {
             case ConsoleLevel.INFO:
-                this._messages.push({ message: `[${dateStr}] [info] ${message}`, level: level });
+                log = { message: `[${dateStr}] [info] ${message}`, level: level };
                 break;
             case ConsoleLevel.WARN:
-                this._messages.push({ message: `[${dateStr}] [warn] ${message}`, level: level });
+                log = { message: `[${dateStr}] [warn] ${message}`, level: level };
                 break;
             case ConsoleLevel.ERROR:
-                this._messages.push({ message: `[${dateStr}] [error] ${message}`, level: level });
+                log = { message: `[${dateStr}] [error] ${message}`, level: level };
                 break;
         }
+
+        // Add message
+        this._messages.push(log);
 
         // Check limit
         if (this._messages.length > EditorConsole.MaxLogsCount)
             this._messages.shift();
 
+        // Check filter
+        if (!this._filters[log.level])
+            return;
+
+        // Add!
         this._addingLog = true;
-        this.code.setValue(this._messages.map(m => m.message).join('\n'));
+        this.code.setValue(this.code.getValue() + (this._messages.length > 1 ? '\n' : '') + this._messages[this._messages.length - 1].message);
         if (this._autoScroll)
             this.code.editor.revealLine(this.code.editor.getModel().getLineCount());
         this._addingLog = false;
@@ -152,8 +149,7 @@ export default class EditorConsole {
 
     // Creates the code editor.
     private async _createEditor (): Promise<void> {
-        this.code = new CodeEditor('consoleLanguage', '');
-        this.code.theme = 'consoleTheme';
+        this.code = new CodeEditor('log', '');
         await this.code.build('CONSOLE-EDITOR');
 
         // Events
@@ -172,6 +168,35 @@ export default class EditorConsole {
             case 'clear':
                 this.clear();
                 break;
+            case 'informations':
+            case 'warnings':
+            case 'errors':
+                const isChecked = this.toolbar.isChecked(id, true);
+                switch (id) {
+                    case 'informations': this._filters[ConsoleLevel.INFO] = isChecked; break;
+                    case 'warnings': this._filters[ConsoleLevel.WARN] = isChecked; break;
+                    case 'errors': this._filters[ConsoleLevel.ERROR] = isChecked; break;
+                }
+                this.toolbar.setChecked(id, isChecked);
+
+                this._filtersChanged();
+                break;
         }
+    }
+
+    // Called on the filters changed.
+    private _filtersChanged (): void {
+        const messages = this._messages.filter(m => {
+            if (!this._filters[m.level])
+                return false;
+
+            return true;
+        });
+
+        this._addingLog = true;
+        this.code.setValue(messages.map(m => m.message).join('\n'));
+        if (this._autoScroll)
+            this.code.editor.revealLine(this.code.editor.getModel().getLineCount());
+        this._addingLog = false;
     }
 }
