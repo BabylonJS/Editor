7da99aa234a1e7f47fa1f2456aa896fb753f5b88
julien-moreau <moreau-mathis@intechinfo.fr>
2021-06-25 02:39:36 +0200

    Fixed node material texture generator

--- src/renderer/editor/tools/components/material.ts
+++ src/renderer/editor/tools/components/material.ts
@@ -30,8 +30,8 @@ export class MaterialTools {
             if (!material.metadata?.editorPath) { continue; }
 
             const forceExportTextures = (material.metadata?.shouldExportTextures ?? true) === true;
-            
-            material.metadata ??= { };
+
+            material.metadata ??= {};
             material.metadata.shouldExportTextures = false;
 
             for (const b of m.blocks ?? []) {
@@ -45,11 +45,14 @@ export class MaterialTools {
                     b.texture.url = b.texture.name;
                     b.texture.name = b.texture.metadata?.editorName ?? Tools.RandomId();
 
-                    const path = join(outputPath, dirname(material.metadata.editorPath));
-                    promises.push(this._ExportSerializedTexture(editor, m, b, block.texture, textureIndex++, path, files, forceExportTextures));
+                    const materialPath = dirname(material.metadata.editorPath);
+                    const path = join(outputPath, materialPath);
+                    promises.push(this._ExportSerializedTexture(editor, m, materialPath, block.texture, textureIndex++, path, files, forceExportTextures).then((n) => {
+                        b.texture.url = b.texture.name = join(dirname(material.metadata.editorPath), n);
+                    }));
                     continue;
                 }
-                
+
                 if (!b.texture.forcedExtension) {
                     b.texture.url = `data:${Tools.RandomId()}`;
                     continue;
@@ -67,25 +70,30 @@ export class MaterialTools {
     /**
      * Exports the given serialized texture.
      */
-    private static async _ExportSerializedTexture(editor: Editor, material: any, block: any, texture: Texture, index: number, path: string, files: string[], forceExportTexture: boolean): Promise<void> {        
-        const extractedTextureName = filenamify(`${material.name}-${material.id}-${index++}.png`);
+    private static async _ExportSerializedTexture(editor: Editor, m: any, materialPath: string, texture: Texture, index: number, path: string, files: string[], forceExportTexture: boolean): Promise<string> {
+        const extractedTextureName = filenamify(`${m.name}-${m.id}-${index++}.png`);
         const destination = join(path, extractedTextureName);
 
         if (!forceExportTexture && await pathExists(destination)) {
             editor.console.logInfo(`NodeMaterial texture named "${extractedTextureName}" already generated.`);
             files.push(extractedTextureName);
-            return;
+            return extractedTextureName;
         }
-        
+
         const buffer = await TextureTools.ConvertTextureToBuffer(texture);
-        if (!buffer) { return; }
-        
+        if (!buffer) {
+            return extractedTextureName;
+        }
+
         files.push(extractedTextureName);
-        await writeFile(destination, new Buffer(buffer));
+        await Promise.all([
+            writeFile(destination, Buffer.from(buffer)),
+            writeFile(join(editor.assetsBrowser.assetsDirectory, materialPath, extractedTextureName), Buffer.from(buffer)),
+        ]);
 
-        block.texture.url = block.texture.name = join("files", extractedTextureName);
+        editor.console.logInfo(`Generated NodeMaterial texture for "${m.name}" at ${extractedTextureName}`);
 
-        editor.console.logInfo(`Generated NodeMaterial texture for "${material.name}" at ${extractedTextureName}`);
+        return extractedTextureName;
     }
 
     /**
