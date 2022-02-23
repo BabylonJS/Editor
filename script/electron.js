14e8d6d0514a639b32f50edb0a54e754ccbecf8f
julien-moreau <moreau-mathis@intechinfo.fr>
2022-02-24 00:02:45 +0100

    chore: update electron build script to include command arguments --x64 and --arm64

--- script/electron.js
+++ script/electron.js
@@ -4,59 +4,75 @@ const Builder = require("electron-builder");
 const yargs = require("yargs");
 const args = yargs.argv;
 
-console.log(`
--------------------------------------------------------------
-ELECTRON BUILD
--------------------------------------------------------------
-`);
+console.log("-------------------------------------------------------------");
+console.log("ELECTRON BUILD");
+console.log("-------------------------------------------------------------");
 console.log("\nBuilding Electron...");
 
-// Arch
-const arch = os.arch();
-const x64 = arch === "x64";
-const arm64 = arch === "arm64";
+const build = ({ x64, arm64 } = options) => {
+    return Builder.build({
+        x64,
+        arm64,
+        config: {
+            fileAssociations: [{
+                ext: "editorproject",
+                name: "Babylon.js Editor Project"
+            }, {
+                ext: "editorworkspace",
+                name: "Babylon.js Editor Workspace"
+            }],
+            appId: "editor.babylonjs.com",
+            productName: "BabylonJS Editor",
+            icon: "./css/icons/babylonjs_icon",
+            directories: {
+                output: "./electron-packages/"
+            },
+            nsis: {
+                oneClick: false
+            },
+            asar: true,
+            compression: "store",
+            extraFiles: [
+                "assets/wizard/**",
+                "assets/project/**",
+                "assets/scripts/**",
+                "assets/graphs/**",
+                "assets/extras/**",
+            ],
+            files: [
+                "src/**",
 
-// Build
-Builder.build({
-    x64,
-    arm64,
-    config: {
-        fileAssociations: [{
-            ext: "editorproject",
-            name: "Babylon.js Editor Project"
-        }, {
-            ext: "editorworkspace",
-            name: "Babylon.js Editor Workspace"
-        }],
-        appId: "editor.babylonjs.com",
-        productName: "BabylonJS Editor",
-        icon: "./css/icons/babylonjs_icon",
-        directories: {
-            output: "./electron-packages/"
-        },
-        nsis: {
-            oneClick: false
-        },
-        asar: true,
-        compression: "store",
-        extraFiles: [
-            "assets/wizard/**",
-            "assets/project/**",
-            "assets/scripts/**",
-            "assets/graphs/**",
-            "assets/extras/**",
-        ],
-        files: [
-            "src/**",
+                "build/**",
+                "declaration/**",
 
-            "build/**",
-            "declaration/**",
+                "assets/**",
+                "css/**",
+                "html/**",
 
-            "assets/**",
-            "css/**",
-            "html/**",
+                "photoshop-extension/**"
+            ]
+        }
+    });
+};
 
-            "photoshop-extension/**"
-        ]
+(async () => {
+    const archs = [
+        { type: "x64", enabled: args.x64 },
+        { type: "arm64", enabled: args.arm64 },
+    ];
+
+    if (archs.find((a) => a.enabled)) {
+        for (const a of archs) {
+            console.log("   -------------------------------------------------------------");
+            console.log(`   ${a.type}`);
+            console.log("   -------------------------------------------------------------");
+            await build({ [a.type]: true });
+        }
+    } else {
+        const arch = os.arch();
+        const x64 = arch === "x64";
+        const arm64 = arch === "arm64";
+
+        await build({ x64, arm64 });
     }
-});
+})();
