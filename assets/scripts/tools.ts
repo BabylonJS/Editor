a737c86e5f0abd3e46f64f61d63ae03cbc290ade
julien-moreau <moreau-mathis@intechinfo.fr>
2022-05-03 19:01:34 +0200

    fix: remove typings error in tools.ts

--- assets/scripts/tools.ts
+++ assets/scripts/tools.ts
@@ -53,7 +53,7 @@ export interface IScript {
      * Called on the object has been disposed.
      * Object can be disposed manually or when the editor stops running the scene.
      */
-    onStop(): void;
+    onStop?(): void;
     /**
      * Called on a message has been received and sent from a graph.
      * @param message defines the name of the message sent from the graph.
