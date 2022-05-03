df85876dbc4ed6156e4cc7c28f4721dff76d8320
julien-moreau <moreau-mathis@intechinfo.fr>
2022-05-03 19:02:09 +0200

    fix: close socket in webpack progress plugin when done #367

--- webpack-extension/src/index.js
+++ webpack-extension/src/index.js
@@ -3,24 +3,38 @@ const net = require("net");
 var socket = null;
 var connected = false;
 
-var connect = function() {
-    socket.connect(6969, "localhost", function() {
+var connect = function () {
+    socket.connect(6969, "localhost", function () {
         console.info("Connected to Babylon.JS Editor to notify progress.");
     });
 };
 
-var notify = function(percentage, message) {
-    if (!socket && percentage === 0) {
+var destroy = function () {
+    try {
+        socket.destroy();
+    } catch (e) {
+        // Catch silently
+    }
+
+    socket = null;
+    connected = false;
+}
+
+var notify = function (percentage, message) {
+    if (!socket && percentage < 1) {
         socket = new net.Socket({ readable: false, writable: true });
-        socket.on("connect", function() {
+
+        socket.on("connect", function () {
             connected = true;
             notify(percentage, message);
         });
-        socket.on("close", function() {
+
+        socket.on("close", function () {
             connected = false;
         });
-        socket.on("error", function() {
-            setTimeout(function() {
+
+        socket.on("error", function () {
+            setTimeout(function () {
                 if (socket) {
                     connect();
                 }
@@ -35,39 +49,36 @@ var notify = function(percentage, message) {
             percentage = 1;
         }
 
-        var done = percentage >= 1;
-
         const data = JSON.stringify({
-            done: done,
             message: message,
+            done: percentage >= 1,
             percentage: percentage * 100,
         });
 
         socket.write(data + "\n");
-
-        if (done) {
-            socket.destroy();
-            socket = null;
-
-            connected = false;
-        }
     }
 };
 
 module.exports = {
-    createProgressPlugin: function(plugin) {
-        if (!plugin) { plugin = { }};
-        
+    createProgressPlugin: function (plugin) {
+        if (!plugin) {
+            plugin = {};
+        }
+
         // Setup progress handler
         var handler = plugin.handler;
-        plugin.handler = function(percentage, message) {
+        plugin.handler = function (percentage, message) {
             if (handler) {
                 handler(percentage, message);
             }
 
             notify(percentage, message);
+
+            if (percentage >= 1) {
+                destroy();
+            }
         };
-        
+
         return plugin;
     },
 };
