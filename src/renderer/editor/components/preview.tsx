a0b650b66e070efdac01b4130fb84725fb2d075c
julien-moreau <moreau-mathis@intechinfo.fr>
2023-02-07 22:30:14 +0100

    feat: add support of camera preview

--- src/renderer/editor/components/preview.tsx
+++ src/renderer/editor/components/preview.tsx
@@ -6,7 +6,8 @@ import * as React from "react";
 import { Position, ButtonGroup, Popover, Menu, MenuItem, Divider, Tag, Tooltip, Pre, AnchorButton, ProgressBar } from "@blueprintjs/core";
 
 import {
-    Node, TargetCamera, Vector3, Animation, Camera, IParticleSystem, ParticleSystem, AbstractMesh, Sound, Observable, ReflectionProbe, Mesh,
+    Node, TargetCamera, Vector3, Animation, Camera, IParticleSystem, ParticleSystem, AbstractMesh, Sound, Observable, ReflectionProbe,
+    Mesh,
 } from "babylonjs";
 
 import { Editor } from "../editor";
@@ -26,6 +27,7 @@ import { SceneExporter } from "../project/scene-exporter";
 import { ScenePlayer } from "../../play/inline-play";
 
 import { PreviewCopyHelper } from "./preview/copy";
+import { CameraPreview } from "./preview/camera-preview";
 
 export enum PreviewFocusMode {
     Target = 1,
@@ -91,6 +93,11 @@ export interface IPreviewState {
      * Defines the current play loading progress.
      */
     playLoadingProgress: number;
+
+    /**
+     * Defines wether or not the camera preview is visible.
+     */
+    cameraPreviewVisible: boolean;
 }
 
 export enum PreviewCanvasEventType {
@@ -131,6 +138,8 @@ export class Preview extends React.Component<IPreviewProps, IPreviewState> {
     private _cameraTargetBeforeIsolation: Nullable<Vector3> = null;
     private _isolationBaseMeshesArray: Nullable<AbstractMesh[]> = null;
 
+    private _cameraPreview: Nullable<CameraPreview> = null;
+
     private _searchBar: Omnibar;
     private _playIframe: HTMLIFrameElement;
     private _refHandler = {
@@ -166,6 +175,7 @@ export class Preview extends React.Component<IPreviewProps, IPreviewState> {
             isPlaying: false,
             isPlayingInIframe: false,
             playLoadingProgress: 1,
+            cameraPreviewVisible: false,
         };
     }
 
@@ -265,9 +275,17 @@ export class Preview extends React.Component<IPreviewProps, IPreviewState> {
                     <div style={{ position: "absolute", top: "50%", left: "25%", width: "50%" }}>
                         {loadingProgress}
                     </div>
+                    <CameraPreview ref={(r) => this._cameraPreview = r} editor={this._editor} />
                 </div>
             </>
         );
+    }
+
+    /**
+     * Called on the component did mount.
+     */
+    public async componentDidMount(): Promise<void> {
+        this._editor.selectedNodeObservable.add((n) => this._cameraPreview?.setSelectedNode(n));
     }
 
     /**
---
+++ src/renderer/editor/components/preview/camera-preview.tsx
@@ -0,0 +1,159 @@
+import { Nullable } from "../../../../shared/types";
+
+import * as React from "react";
+
+import { Camera, Node, Observer, RenderTargetTexture } from "babylonjs";
+
+import { Editor } from "../../editor";
+import { Button } from "@blueprintjs/core";
+
+export interface ICameraPreviewProps {
+	editor: Editor;
+}
+
+export interface ICameraPreviewState {
+	visible: boolean;
+}
+
+export class CameraPreview extends React.Component<ICameraPreviewProps, ICameraPreviewState> {
+	private _selectedNode: Nullable<Camera> = null;
+	private _canvas: Nullable<HTMLCanvasElement> = null;
+	private _renderTarget: Nullable<RenderTargetTexture> = null;
+	private _viewMatrixChangeObserver: Nullable<Observer<Camera>> = null;
+	private _projectionMatrixChangeObserver: Nullable<Observer<Camera>> = null;
+
+	/**
+	 * Constructor.
+	 * @param props defines the component's props.
+	 */
+	public constructor(props: ICameraPreviewProps) {
+		super(props);
+
+		this.state = {
+			visible: false,
+		};
+	}
+
+	/**
+	 * Renders the component.
+	 */
+	public render(): React.ReactNode {
+		return (
+			<>
+				<div style={{ width: "400px", height: "24px", position: "absolute", right: "10px", bottom: "210px", textAlign: "center", lineHeight: "24px", display: this.state.visible ? "" : "none", background: "#00000044", backdropFilter: "blur(50px)" }}>
+					<span>Preview: {this._selectedNode?.name}</span>
+					<Button text="x" small style={{ position: "absolute", right: "2px", top: "0px", borderRadius: "100%" }} onClick={() => this._clear()} />
+				</div>
+				<div style={{ width: "400px", height: "200px", position: "absolute", right: "10px", bottom: "10px", pointerEvents: "none", display: this.state.visible ? "" : "none", background: "#00000044", backdropFilter: "blur(50px)" }}>
+					<canvas ref={(r) => this._canvas = r} style={{ width: "100%", height: "100%", transform: "scale(1, -1)", objectFit: "contain", pointerEvents: "none" }} />
+				</div>
+			</>
+		);
+	}
+
+	/**
+	 * Sets the selected in the editor. In case of a camera, create the preview.
+	 * @param node defines the reference to the selected node in the editor.
+	 */
+	public setSelectedNode(node: Node): void {
+		this._clear();
+
+		if (!(node instanceof Camera)) {
+			return;
+		}
+
+		this.setState({ visible: true });
+
+		this._selectedNode = node;
+
+		this._renderTarget = new RenderTargetTexture("camera_preview_rtt", { width: 400, height: 200 }, this.props.editor.scene!, true, true);
+		this._renderTarget.refreshRate = 0;
+		this._renderTarget.activeCamera = node;
+		this._renderTarget.useCameraPostProcesses = false;
+		this._renderTarget.renderList = this.props.editor.scene!.meshes.slice();
+
+		this.props.editor.scene!.customRenderTargets.push(this._renderTarget);
+
+		const c = this._canvas?.getContext("2d");
+		if (!c) {
+			return;
+		}
+
+		let computing = false;
+		let firstRender = true;
+		let buffer = new Uint8Array();
+
+		this._viewMatrixChangeObserver = node.onViewMatrixChangedObservable.add(() => {
+			this._renderTarget!.refreshRate = 0;
+		});
+
+		this._projectionMatrixChangeObserver = node.onProjectionMatrixChangedObservable.add(() => {
+			this._renderTarget!.refreshRate = 0;
+		});
+
+		this._renderTarget.onAfterRenderObservable.add(async () => {
+			if (computing) {
+				return;
+			}
+
+			computing = true;
+
+			const renderWidth = this.props.editor.engine!.getRenderWidth();
+			const renderHeight = this.props.editor.engine!.getRenderHeight();
+
+			const { width, height } = this._renderTarget!.getSize();
+
+			if (width !== this.props.editor.engine!.getRenderWidth() || height !== this.props.editor.engine!.getRenderHeight()) {
+				this._renderTarget!.resize({ width: renderWidth, height: renderHeight });
+			}
+
+			if (c.canvas.width !== width || c.canvas.height !== height) {
+				c.canvas.width = width;
+				c.canvas.height = height;
+			}
+
+			const count = width * height * 4;
+			if (buffer.length !== count) {
+				buffer = new Uint8Array(count);
+			}
+
+			try {
+				const p = await this._renderTarget!.readPixels(0, 0, buffer, true, true) as Uint8Array;
+				const a = new ImageData(new Uint8ClampedArray(p), width, height);
+
+				c.scale(1, -1);
+				c.putImageData(a, 0, 0);
+			} catch (e) {
+				// Catch silently.
+			}
+
+			computing = false;
+
+			if (firstRender) {
+				firstRender = false;
+				this._renderTarget!.refreshRate = 0;
+			}
+		});
+	}
+
+	/**
+	 * Clears all the preview allocated resources.
+	 */
+	private _clear(): void {
+		if (this._renderTarget) {
+			this._renderTarget?.dispose();
+			this._renderTarget = null;
+		}
+
+		if (this._selectedNode) {
+			this._selectedNode.onViewMatrixChangedObservable.remove(this._viewMatrixChangeObserver);
+			this._selectedNode.onProjectionMatrixChangedObservable.remove(this._projectionMatrixChangeObserver);
+
+			this._viewMatrixChangeObserver = null;
+			this._projectionMatrixChangeObserver = null;
+		}
+
+		this._selectedNode = null;
+		this.setState({ visible: false });
+	}
+}
