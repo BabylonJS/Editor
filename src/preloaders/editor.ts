88cf1765dc36ff6e98538045cb3be0669883e7d0
Brendon Smith <brendonsmith@seacloud9.org>
2023-05-15 14:56:49 -0700

    adding missed changes

--- src/preloaders/editor.ts
+++ src/preloaders/editor.ts
@@ -1,9 +1,14 @@
 import { join, resolve, basename } from "path";
 
+import { Tools } from "../renderer/editor/tools/tools";
+
 /**
  * When href contains editor.html, let's load the editor itself.
  */
 function runEditor(): void {
+    // Require modules hack for development mode.
+    require('../renderer/module.js');
+
     window["CANNON"] = require("cannon");
 
     const Editor = require('../renderer/editor/index.js');
@@ -39,6 +44,8 @@ function runEditor(): void {
  * When href contains play.html, let's run the project in an isolated iframe.
  */
 function runIsolatedPlay(): void {
+    require('../renderer/module.js');
+
     window.addEventListener("message", function (ev) {
         if (ev.data?.id !== "init") {
             return;
@@ -55,10 +62,6 @@ function runIsolatedPlay(): void {
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-    // Require modules hack for development mode.
-    require('../renderer/module.js');
-
-    const { Tools } = require("../renderer/editor/tools/tools");
     await Tools.Wait(100);
 
     const htmlFile = basename(window.location.href);
