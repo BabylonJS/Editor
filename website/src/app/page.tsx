5c53d40939ab32dbe8f24708290d33f5ee6b3aeb
julien-moreau <moreau-mathis@intechinfo.fr>
2024-06-21 00:38:19 +0200

    feat: preparing website

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <ReactLenis root>
 
             <main className="min-w-screen min-h-screen landing text-black dark:text-white">
-                <div className={`fixed top-0 left-0 w-screen h-screen z-0 ${section2Visible ? "" : "blur-xl scale-125 opacity-50"} transition-all duration-1000 ease-in-out`}>
+                <div className={`fixed top-0 left-0 w-screen h-screen z-0 ${section2Visible ? "" : "blur-xl scale-125 opacity-50 brightness-0"} transition-all duration-1000 ease-in-out`}>
                     <RendererComponent />
                 </div>
 
@@ -31,24 +31,32 @@ export default function Home() {
                     <img alt="" src="/logo.svg" className="h-14 lg:h-20 -ml-5" />
                 </div>
 
-                <div className="flex flex-col justify-center items-center gap-5 w-screen h-screen max-w-3xl px-5 mx-auto">
-                    <div className="text-4xl md:text-7xl">
+                <div className="flex flex-col justify-between items-center gap-5 w-screen h-screen max-w-7xl pt-48 px-5 mx-auto">
+                    <Fade cascade damping={0.1} triggerOnce direction="up">
                         <Fade>
-                            Babylon.JS Editor
+                            <div className="text-4xl md:text-7xl lg:text-9xl font-semibold font-sans">
+                                Babylon.js Editor
+                            </div>
                         </Fade>
-                    </div>
 
-                    <div className="text-xl text-center">
                         <Fade>
-                            Focus more on creating and less on coding.
+                            <div className="text-xl md:text-3xl text-center max-w-64 md:max-w-max font-semibold">
+                                Focus more on <b className="text-[rgb(73,202,146)]">creating</b> and less on <b className="text-[rgb(218,151,55)]">coding</b>.
+                            </div>
                         </Fade>
-                    </div>
 
-                    <div className="text-center text-sm max-w-3xl mx-auto">
                         <Fade>
-                            The Babylon.JS Editor is an open source project maintained by the community. The mission is to provide community-driven powerful and simple tools that help Babylon.JS users to create beautiful, awesome 3D games / applications. It comes with deep customization features and is built using Electron to support cross-platform development. Using the latest version of Babylon.JS, the Editor allows creating highly customizable 3D web project skeletons based on the powerful ES6 modules version of Babylon.JS.
+                            <div className="text-center text-sm lg:text-lg max-w-3xl mx-auto">
+                                The mission is to provide community-driven powerful and simple tools that help Babylon.JS users to create beautiful, awesome 3D games / applications.
+                            </div>
                         </Fade>
-                    </div>
+
+                        <Fade>
+                            <img alt="" src="/screenshots/large.png" className="w-full h-full object-contain z-50 hidden sm:hidden md:hidden lg:hidden xl:block" />
+                            <img alt="" src="/screenshots/medium.png" className="w-full h-full object-contain z-50 hidden sm:hidden md:hidden lg:block xl:hidden" />
+                            <img alt="" src="/screenshots/small.png" className="w-full h-full object-contain z-50 lg:hidden" />
+                        </Fade>
+                    </Fade>
                 </div>
 
                 <div className="flex flex-col justify-between w-screen h-screen max-w-3xl px-5 mx-auto" ref={section2Ref}>
@@ -83,6 +91,6 @@ export default function Home() {
                     </div>
                 </div>
             </main>
-        </ReactLenis>
+        </ReactLenis >
     );
 }
