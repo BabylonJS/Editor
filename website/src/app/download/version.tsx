2814697751deddc09029e897171e7f1a76fc5d29
Julien Moreau-Mathis <julien.moreaumathis@nextdecade.io>
2024-06-29 00:06:36 +0200

    feat: add 4.7.0 download

---
+++ website/src/app/download/version.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+
+import { Fade } from "react-awesome-reveal";
+
+import { AppleIcon } from "@/components/icons/apple";
+import { WindowsIcon } from "@/components/icons/windows";
+
+export interface IDownloadVersionComponentProps {
+    version: string;
+
+    windowsLink: string;
+
+    macArm64Link: string;
+    macIntelLink: string;
+}
+
+export function DownloadVersionComponent(props: IDownloadVersionComponentProps) {
+    return (
+        <>
+            <Fade>
+                <div className="text-3xl font-semibold font-sans tracking-tighter text-neutral-500 text-center">
+                    {props.version}
+                </div>
+            </Fade>
+
+            <Fade className="w-full">
+                <div className="flex w-full">
+                    <div className="flex flex-col gap-4 items-center w-full">
+                        <WindowsIcon color="#fff" className="w-32 h-32" />
+
+                        <Link href={props.windowsLink} className="font-semibold font-sans tracking-tighter underline underline-offset-2">
+                            Windows 64bits Installer
+                        </Link>
+                    </div>
+
+                    <div className="flex flex-col gap-4 items-center w-full">
+                        <AppleIcon color="#fff" className="w-32 h-32" />
+
+                        <Link href={props.macArm64Link} className="font-semibold font-sans tracking-tighter underline underline-offset-2">
+                            macOS Apple Silicon
+                        </Link>
+
+                        <Link href={props.macIntelLink} className="font-semibold font-sans tracking-tighter underline underline-offset-2">
+                            macOS Intel Chip
+                        </Link>
+                    </div>
+                </div>
+            </Fade>
+        </>
+    );
+}
