d929be19665156e7a9591601d39e130a1e1b6dcf
Maki Nada <moreau-mathis@intechinfo.fr>
2024-07-15 13:35:08 +0200

    feat: add support of refresh rate for classic shadow maps

--- editor/src/tools/light/shadows.ts
+++ editor/src/tools/light/shadows.ts
@@ -1,4 +1,4 @@
-import { Light, Vector3 } from "babylonjs";
+import { Light, RenderTargetTexture, Vector3 } from "babylonjs";
 
 import { isPointLight } from "../guards/nodes";
 
@@ -22,4 +22,20 @@ export function updatePointLightShadowMapRenderListPredicate(light: Light): void
         const distance = Vector3.Distance(mesh.getAbsolutePosition(), light.getAbsolutePosition());
         return distance <= light.range;
     };
+}
+
+/**
+ * In case the light has a refresh rate of 0, let's update them to reset refresh rate to
+ * 0 in order to re-trigger a render of the shadow map. This is typically used when a light is moved in the editor.
+ * @param light defines the reference to the light to configure.
+ */
+export function updateLightShadowMapRefreshRate(light: Light): void {
+    const shadowMap = light.getShadowGenerator()?.getShadowMap();
+    if (!shadowMap) {
+        return;
+    }
+
+    if (shadowMap.refreshRate === RenderTargetTexture.REFRESHRATE_RENDER_ONCE) {
+        shadowMap.refreshRate = RenderTargetTexture.REFRESHRATE_RENDER_ONCE;
+    }
 }
