57d5313c9dff8292be0438c331450c06b8c23096
Makiki <moreau-mathis@intechinfo.fr>
2024-07-31 15:00:04 +0200

    chore: bump editor to v5.0.0-alpha.4

--- website/src/app/download/page.tsx
+++ website/src/app/download/page.tsx
@@ -27,7 +27,7 @@ export default function DownloadPage() {
                     </Fade>
 
                     <DownloadVersionComponent
-                        version="v5.0.0-alpha.3"
+                        version="v5.0.0-alpha.4"
                         windowsLink="https://babylonjs-editor.fra1.cdn.digitaloceanspaces.com/BabylonJS%20Editor%20Setup%205.0.0.exe"
                         macArm64Link="https://babylonjs-editor.fra1.cdn.digitaloceanspaces.com/BabylonJS%20Editor-5.0.0-arm64.dmg"
                         macIntelLink="https://babylonjs-editor.fra1.cdn.digitaloceanspaces.com/BabylonJS%20Editor-5.0.0.dmg"
