cecc811bbe3fc413dfc1f25ccce5b75b89f84e1d
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2024-09-12 11:29:01 +0200

    feat: starting basics of animation editor

--- editor/src/editor/layout/animation.tsx
+++ editor/src/editor/layout/animation.tsx
@@ -1,10 +1,22 @@
 import { Component, ReactNode } from "react";
 
+import { EditorAnimationToolbar } from "./animation/toolbar";
+import { EditorAnimationTimelinePanel } from "./animation/timeline";
+import { EditorAnimationPropertiesPanel } from "./animation/properties";
+
 export class EditorAnimation extends Component {
     public render(): ReactNode {
         return (
-            <div className="flex justify-center items-center w-full h-full font-semibold text-3xl">
-                Coming soon
+            <div className="flex flex-col w-full h-full">
+                <EditorAnimationToolbar />
+
+                <div className="flex w-full h-full">
+                    <EditorAnimationPropertiesPanel />
+
+                    <div className="w-1 h-full bg-primary-foreground" />
+
+                    <EditorAnimationTimelinePanel />
+                </div>
             </div>
         );
     }
---
+++ editor/src/editor/layout/animation/toolbar.tsx
@@ -0,0 +1,30 @@
+import { Component, ReactNode } from "react";
+
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from "../../../ui/shadcn/ui/menubar";
+
+export class EditorAnimationToolbar extends Component {
+    public render(): ReactNode {
+        return (
+            <div className="w-full h-10 bg-primary-foreground">
+                <Menubar className="border-none rounded-none pl-3 my-auto bg-primary-foreground">
+                    {/* File */}
+                    <MenubarMenu>
+                        <MenubarTrigger disabled>
+                            File
+                        </MenubarTrigger>
+
+                        <MenubarContent className="border-black/50">
+                            <MenubarItem>
+                                Load Animation From...
+                            </MenubarItem>
+                            <MenubarSeparator />
+                            <MenubarItem>
+                                Save Animation As...
+                            </MenubarItem>
+                        </MenubarContent>
+                    </MenubarMenu>
+                </Menubar>
+            </div>
+        );
+    }
+}
