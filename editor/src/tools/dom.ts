473c91625bddeb9d1ed20036ac8a342ee1bb7da1
julien-moreau <moreau-mathis@intechinfo.fr>
2024-09-25 22:38:36 +0200

    feat: add context menu support to add keys

--- editor/src/tools/dom.ts
+++ editor/src/tools/dom.ts
@@ -12,3 +12,20 @@ export function isDomElementFocusable(element: Element | null | undefined): bool
     const inputs = ["input", "select", "button", "textarea"];
     return inputs.includes(element.tagName.toLowerCase());
 }
+
+/**
+ * Returns wether or not the given DOM element is a descendant of the given parent.
+ * @param element defines the reference to the DOM element to check.
+ * @param parent defines the reference to the parent DOM element to check against.
+ */
+export function isDomElementDescendantOf(element: HTMLElement, parent: HTMLElement): boolean {
+    while (element) {
+        if (element === parent) {
+            return true;
+        }
+
+        element = element.parentElement!;
+    }
+
+    return false;
+}
