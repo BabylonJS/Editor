d6e3328fd3c59e7ea8feba2ed6afab6a310ccc21
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-01-30 12:50:02 +0100

    fix: size of toolbar for window in dashboard

--- editor/src/dashboard/window-controls.tsx
+++ editor/src/dashboard/window-controls.tsx
@@ -1,4 +1,3 @@
-import { platform } from "os";
 import { ipcRenderer } from "electron";
 
 import { IoCloseOutline } from "react-icons/io5";
@@ -6,26 +5,26 @@ import { VscChromeMinimize, VscMultipleWindows } from "react-icons/vsc";
 
 import { Button } from "../ui/shadcn/ui/button";
 
-const isWin32 = platform() === "win32";
+import { isDarwin } from "../tools/os";
 
 export function WindowControls() {
     return (
-        <div className={`absolute top-0 left-0 w-full ${isWin32 ? "h-28" : "h-24"}`}>
+        <div className={`absolute top-0 left-0 w-full ${!isDarwin() ? "h-28" : "h-24"}`}>
             <div className="flex w-full h-full">
                 <div className="w-full electron-draggable h-full" />
 
-                {isWin32 &&
+                {(!isDarwin() || process.env.DEBUG) &&
                     <div className="flex z-50">
-                        <Button variant="ghost" className="w-10 aspect-square p-0 hover:bg-muted" onClick={() => ipcRenderer.send("window:minimize")}>
-                            <VscChromeMinimize className="w-4 h-4" />
+                        <Button variant="ghost" className="w-12 aspect-square !p-0 hover:bg-muted" onClick={() => ipcRenderer.send("window:minimize")}>
+                            <VscChromeMinimize className="w-5 h-5" />
                         </Button>
 
-                        <Button variant="ghost" className="w-10 aspect-square p-3.5 hover:bg-muted" onClick={() => ipcRenderer.send("window:maximize")}>
-                            <VscMultipleWindows className="w-4 h-4" />
+                        <Button variant="ghost" className="w-12 aspect-square !p-4 hover:bg-muted" onClick={() => ipcRenderer.send("window:maximize")}>
+                            <VscMultipleWindows className="w-5 h-5" />
                         </Button>
 
-                        <Button variant="ghost" className="w-10 aspect-square p-0 hover:bg-muted" onClick={() => ipcRenderer.send("window:close")}>
-                            <IoCloseOutline className="w-4 h-4" />
+                        <Button variant="ghost" className="w-12 aspect-square !p-0 hover:bg-muted" onClick={() => ipcRenderer.send("window:close")}>
+                            <IoCloseOutline className="w-5 h-5" />
                         </Button>
                     </div>
                 }
