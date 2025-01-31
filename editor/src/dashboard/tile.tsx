ff768d31ad3ba09b661d1330be081fec7d649314
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-01-31 15:48:47 +0100

    feat: add way to quickly launch project via dashboard

--- editor/src/dashboard/tile.tsx
+++ editor/src/dashboard/tile.tsx
@@ -1,13 +1,19 @@
-import { ipcRenderer } from "electron";
+import { ipcRenderer, shell } from "electron";
 import { basename, dirname } from "path/posix";
+
+import { useEffect, useState } from "react";
+import { Grid } from "react-loader-spinner";
 
 import { FaQuestion } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
+import { IoPlay, IoStop } from "react-icons/io5";
 
+import { Button } from "../ui/shadcn/ui/button";
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuTrigger } from "../ui/shadcn/ui/context-menu";
 
 import { isDarwin } from "../tools/os";
 import { ProjectType } from "../tools/project";
+import { execNodePty, NodePtyInstance } from "../tools/node-pty";
 
 export interface IProjectTileProps {
     isOpened: boolean;
@@ -17,14 +23,61 @@ export interface IProjectTileProps {
 }
 
 export function ProjectTile(props: IProjectTileProps) {
+    const [launching, setLaunching] = useState(false);
+
+    const [playingAddress, setPlayingAddress] = useState("");
+    const [nodePtyInstance, setNodePtyInstance] = useState<NodePtyInstance | null>(null);
+
+    useEffect(() => {
+        return () => {
+            nodePtyInstance?.kill();
+        };
+    }, [nodePtyInstance]);
+
+    useEffect(() => {
+        if (playingAddress) {
+            shell.openExternal(playingAddress);
+        }
+    }, [playingAddress]);
+
+    async function handleLaunchProject() {
+        setLaunching(true);
+
+        const p = await execNodePty("yarn dev", {
+            cwd: dirname(props.project.absolutePath),
+        });
+
+        const observable = p.onGetDataObservable.add((data) => {
+            const localhostRegex = /http:\/\/localhost:(\d+)/;
+            const match = data.match(localhostRegex);
+            if (match) {
+                p.onGetDataObservable.remove(observable);
+
+                setLaunching(false);
+                setPlayingAddress(`http://localhost:${match[1]}`);
+            }
+        });
+
+        setNodePtyInstance(p);
+    }
+
+    async function handleStopProject() {
+        nodePtyInstance?.kill();
+        setNodePtyInstance(null);
+
+        setLaunching(false);
+        setPlayingAddress("");
+    }
+
     return (
         <ContextMenu>
             <ContextMenuTrigger>
                 <div
                     onDoubleClick={() => ipcRenderer.send("dashboard:open-project", props.project.absolutePath)}
                     className={`
+                        group
                         flex flex-col w-full rounded-lg cursor-pointer select-none
-                        ring-muted-foreground hover:ring-2 hover:p-1
+                        ring-muted-foreground hover:ring-2
                         transition-all duration-300 ease-in-out
                         ${props.isOpened ? "opacity-15 pointer-events-none" : ""}
                     `}
@@ -39,8 +92,28 @@ export function ProjectTile(props: IProjectTileProps) {
                     </div>
 
                     <div className="flex flex-col gap-1 p-2 bg-secondary rounded-b-lg select-none">
-                        <div className="text-lg font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
-                            {basename(dirname(props.project.absolutePath))}
+                        <div className="flex justify-between items-center">
+                            <div className="text-lg font-semibold text-ellipsis overflow-hidden whitespace-nowrap">
+                                {basename(dirname(props.project.absolutePath))}
+                            </div>
+
+                            <Button
+                                variant="ghost"
+                                onClick={() => playingAddress ? handleStopProject() : handleLaunchProject()}
+                                className={`
+                                    w-10 h-10 aspect-square p-0
+                                    ${launching || playingAddress ? "" : "opacity-0 group-hover:opacity-100"}
+                                    ${launching ? "bg-muted/50" : playingAddress ? "!bg-red-500/35" : "hover:!bg-green-500/35"}
+                                    transition-all duration-300 ease-in-out
+                                `}
+                            >
+                                {launching
+                                    ? <Grid width={24} height={24} color="#ffffff" />
+                                    : playingAddress
+                                        ? <IoStop className="w-6 h-6" strokeWidth={1} color="red" />
+                                        : <IoPlay className="w-6 h-6" strokeWidth={1} color="green" />
+                                }
+                            </Button>
                         </div>
                         <div className="text-muted-foreground text-xs">
                             Created {new Date(props.project.createdAt).toLocaleString("en-US", { day: "2-digit", month: "long", year: "numeric" })}
