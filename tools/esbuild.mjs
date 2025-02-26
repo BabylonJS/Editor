fe80bdd43a2855ada0163fcbd89074ac41b6e96b
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-02-26 12:39:54 +0100

    chore: babylonjs-editor-tools is now require-able by the editor using CommonJS.

---
+++ tools/esbuild.mjs
@@ -0,0 +1,65 @@
+import esbuild from "esbuild";
+
+import { argv, exit } from "node:process";
+import { readFile } from "node:fs/promises";
+
+const replaceBabylonJsImports = {
+    name: "replaceImportMetaDirname",
+    setup(build) {
+        build.onLoad({ filter: /.*/ }, async (args) => {
+            const source = await readFile(args.path, "utf8");
+
+            const transformedSource = source
+                .replace(/"@babylonjs\/core\/.*"/g, "\"babylonjs\"")
+                .replace(/"@babylonjs\/gui\/.*"/g, "\"babylonjs-gui\"");
+
+            return {
+                loader: "default",
+                contents: transformedSource,
+            };
+        });
+    },
+};
+
+const mainBuildOptions = {
+    entryPoints: [
+        "./src/index.ts",
+    ],
+    bundle: true,
+    platform: "node",
+    target: "node20", // target version of Node.js
+    format: "cjs", // output format as CommonJS
+    outfile: "./build/index.node.js",
+    treeShaking: false,
+    loader: {
+        ".ts": "ts",
+    },
+    external: [
+        "babylonjs",
+        "babylonjs-gui",
+    ],
+    keepNames: true,
+    plugins: [
+        replaceBabylonJsImports
+    ],
+};
+
+const args = argv.slice(2);
+
+if (args.includes("--watch")) {
+    esbuild
+        .context(mainBuildOptions)
+        .then(async (buildcontext) => {
+            await buildcontext.watch();
+            console.log("Watching...");
+        })
+        .catch((error) => {
+            console.error(error);
+            exit(1);
+        });
+} else {
+    esbuild.build(mainBuildOptions).catch((error) => {
+        console.error(error);
+        exit(1);
+    });
+}
