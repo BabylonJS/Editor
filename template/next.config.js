016cc7a453ccd58e828b174dd2c6b609edb07e0a
julien-moreau <moreau-mathis@intechinfo.fr>
2025-03-15 12:57:41 +0100

    chore: update Babylon.js to v7.53.3

--- template/next.config.js
+++ template/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.fx?$/,
+            loader: "raw-loader"
+        });
+        return config;
+    },
 };
 
 module.exports = nextConfig;
