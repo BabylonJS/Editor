6c754ba1da084591851e4a7c892bf8393bae5fc1
Cesharpe <moreau-mathis@intechinfo.fr>
2025-06-28 16:50:54 +0200

    docs: beginning "tips" section in documentation

--- website/src/app/documentation/sidebar/sidebar.tsx
+++ website/src/app/documentation/sidebar/sidebar.tsx
@@ -7,7 +7,7 @@ export function DocumentationSidebar() {
 		<div className="fixed top-0 left-0 w-96 h-screen pt-32 px-5 border-r border-r-neutral-950 text-white">
 			<div className="flex flex-col gap-1">
 				<div className="font-semibold text-xl text-neutral-500 my-3">
-                    Basics
+					Basics
 				</div>
 
 				<DocumentationSidebarItem title="Introduction" href="/documentation" />
@@ -18,12 +18,18 @@ export function DocumentationSidebar() {
 				<DocumentationSidebarItem title="Adding scripts" href="/documentation/adding-scripts" />
 
 				<div className="font-semibold text-xl text-neutral-500 my-3">
-                    Advanced
+					Advanced
 				</div>
 
 				<DocumentationSidebarItem title="Compressing textures" href="/documentation/advanced/compressing-textures" />
 				<DocumentationSidebarItem title="LOD collisions" href="/documentation/advanced/lod-collisions" />
 				<DocumentationSidebarItem title="Optimizing shadows" href="/documentation/advanced/optimizing-shadows" />
+
+				<div className="font-semibold text-xl text-neutral-500 my-3">
+					Tips
+				</div>
+
+				<DocumentationSidebarItem title="Creating a Skybox" href="/documentation/tips/creating-skybox" />
 			</div>
 		</div>
 	);
