8a44256660c97fa01d039bc544e9f8e917b0f034
julien-moreau <moreau-mathis@intechinfo.fr>
2025-08-09 01:06:23 +0200

    style: fix formatting

--- editor/src/electron/events/shell.ts
+++ editor/src/electron/events/shell.ts
@@ -2,8 +2,8 @@ import { platform } from "os";
 import { ipcMain, shell } from "electron";
 
 ipcMain.on("editor:trash-items", async (ev, items) => {
-    const isWindows = platform() === "win32";
-    items = items.map((item) => (isWindows ? item.replace(/\//g, "\\") : item.replace(/\\/g, "/")));
+	const isWindows = platform() === "win32";
+	items = items.map((item) => (isWindows ? item.replace(/\//g, "\\") : item.replace(/\\/g, "/")));
 
 	try {
 		await Promise.all(items.map((item) => shell.trashItem(item)));
@@ -15,8 +15,8 @@ ipcMain.on("editor:trash-items", async (ev, items) => {
 });
 
 ipcMain.on("editor:show-item", (_, item) => {
-    const isWindows = platform() === "win32";
-    item = isWindows ? item.replace(/\//g, "\\") : item.replace(/\\/g, "/");
+	const isWindows = platform() === "win32";
+	item = isWindows ? item.replace(/\//g, "\\") : item.replace(/\\/g, "/");
 
 	shell.showItemInFolder(item);
 });
