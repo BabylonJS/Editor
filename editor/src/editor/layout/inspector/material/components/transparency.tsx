62a81ee7340adfcbf2f6848e813b36f222b87014
Yuri Pourre <yuripourre@gmail.com>
2025-08-10 19:08:06 -0700

    Refactor EditorTransparencyModeField

--- editor/src/editor/layout/inspector/material/components/transparency.tsx
+++ editor/src/editor/layout/inspector/material/components/transparency.tsx
@@ -2,22 +2,25 @@ import { ReactNode } from "react";
 import { EditorInspectorListField } from "../../fields/list";
 import { Material, StandardMaterial, PBRMaterial } from "babylonjs";
 
-
 export interface IEditorTransparencyModeFieldProps {
 	object: StandardMaterial | PBRMaterial;
 	onChange?: () => void;
 }
 
 export function EditorTransparencyModeField(props: IEditorTransparencyModeFieldProps): ReactNode {
-	const getTransparencyModeItems = () => {
-		return [
-			{ text: "None", value: null },
-			{ text: "Opaque", value: Material.MATERIAL_OPAQUE },
-			{ text: "Alpha Test", value: Material.MATERIAL_ALPHATEST },
-			{ text: "Alpha Blend", value: Material.MATERIAL_ALPHABLEND },
-			{ text: "Alpha Test and Blend", value: Material.MATERIAL_ALPHATESTANDBLEND },
-		];
-	};
-
-	return <EditorInspectorListField label="Transparency Mode" object={props.object} property="transparencyMode" onChange={props.onChange} items={getTransparencyModeItems()} />;
+	return (
+		<EditorInspectorListField
+			label="Transparency Mode"
+			object={props.object}
+			property="transparencyMode"
+			onChange={props.onChange}
+			items={[
+				{ text: "None", value: null },
+				{ text: "Opaque", value: Material.MATERIAL_OPAQUE },
+				{ text: "Alpha Test", value: Material.MATERIAL_ALPHATEST },
+				{ text: "Alpha Blend", value: Material.MATERIAL_ALPHABLEND },
+				{ text: "Alpha Test and Blend", value: Material.MATERIAL_ALPHATESTANDBLEND },
+			]}
+		/>
+	);
 }
