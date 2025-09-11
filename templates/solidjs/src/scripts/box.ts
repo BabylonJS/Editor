725d4f55c82807f2f640d8fe6528b4b48c8b0da9
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-09-11 15:28:01 +0200

    fix: use this.mesh.rotate method to rotate mesh in script example of templates To avoid usage of distinct .rotation and .rotationQuaternion properties

--- templates/solidjs/src/scripts/box.ts
+++ templates/solidjs/src/scripts/box.ts
@@ -1,21 +1,20 @@
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 
 import { IScript, visibleAsNumber } from "babylonjs-editor-tools";
 
 export default class SceneComponent implements IScript {
-    @visibleAsNumber("Speed", {
-    	min: 0,
-    	max: 0.1,
-    })
+	@visibleAsNumber("Speed", {
+		min: 0,
+		max: 0.1,
+	})
 	private _speed: number = 0.04;
 
-    public constructor(public mesh: Mesh) { }
+	public constructor(public mesh: Mesh) {}
 
-    public onStart(): void {
+	public onStart(): void {}
 
-    }
-
-    public onUpdate(): void {
-    	this.mesh.rotation.y += this._speed * this.mesh.getScene().getAnimationRatio();
-    }
+	public onUpdate(): void {
+		this.mesh.rotate(Vector3.UpReadOnly, this._speed * this.mesh.getScene().getAnimationRatio());
+	}
 }
