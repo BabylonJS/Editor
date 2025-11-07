41ca24442c86197bdbda0a37cb263d17add1bdd3
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-11-07 17:32:17 +0100

    fix: always save editor data for node material and keep uniqueIds to be fixed #721

--- editor/src/tools/material/material.ts
+++ editor/src/tools/material/material.ts
@@ -1,4 +1,4 @@
-import { Material } from "babylonjs";
+import { Material, NodeMaterial } from "babylonjs";
 
 import { isNodeMaterial, isPBRMaterial, isStandardMaterial } from "../guards/material";
 
@@ -10,4 +10,35 @@ export function configureSimultaneousLightsForMaterial(material: Material) {
 	if (isPBRMaterial(material) || isStandardMaterial(material) || isNodeMaterial(material)) {
 		material.maxSimultaneousLights = 32;
 	}
+}
+
+/**
+ * Normalizes the unique IDs of the nodes in the given Node Material based on the provided material data.
+ * This allows to get ids not modified after reloading the material to be git-compliant.
+ */
+export function normalizeNodeMaterialUniqueIds(material: NodeMaterial, materialData: any) {
+	material.editorData = materialData.editorData;
+	material.attachedBlocks.forEach((block, index) => {
+		const oldUniqueId = block.uniqueId;
+		const newUniqueId = materialData.blocks[index].id;
+
+		material.editorData?.locations?.forEach((location) => {
+			if (location.blockId === oldUniqueId) {
+				location.blockId = newUniqueId;
+			}
+		});
+
+		for (const key in material.editorData?.map ?? {}) {
+			if (!material.editorData?.map.hasOwnProperty(key)) {
+				continue;
+			}
+
+			const value = material.editorData.map[key];
+			if (value === oldUniqueId) {
+				material.editorData.map[key] = newUniqueId;
+			}
+		}
+
+		block.uniqueId = newUniqueId;
+	});
 }
