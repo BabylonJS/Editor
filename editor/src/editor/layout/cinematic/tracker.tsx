c55bf2735ad13697b62647242aadcab463b67ee3
julien-moreau <moreau-mathis@intechinfo.fr>
2025-11-15 01:01:39 +0100

    fix: avoid full state update when playing cinematic inline by updating only time tracker

---
+++ editor/src/editor/layout/cinematic/tracker.tsx
@@ -0,0 +1,35 @@
+import { Component, ReactNode } from "react";
+
+export interface ICinematicEditorTrackerProps {
+	scale: number;
+	currentTime: number;
+}
+
+export class CinematicEditorTracker extends Component<ICinematicEditorTrackerProps> {
+	private _forcedCurrentTime: number | null = null;
+
+	public render(): ReactNode {
+		const currentTime = this._forcedCurrentTime ?? this.props.currentTime;
+
+		return (
+			<div
+				style={{
+					left: `${currentTime * this.props.scale}px`,
+				}}
+				className="absolute w-[1px] ml-2 mt-10 bg-muted h-full pointer-events-none"
+			>
+				<div
+					className="absolute w-7 h-7 rotate-45 -translate-x-1/2 -translate-y-8 bg-muted"
+					style={{
+						mask: "linear-gradient(135deg, transparent 0%, transparent 50%, black 50%, black 100%)",
+					}}
+				/>
+			</div>
+		);
+	}
+
+	public setForcedCurrentTime(time: number | null): void {
+		this._forcedCurrentTime = time;
+		this.forceUpdate();
+	}
+}
