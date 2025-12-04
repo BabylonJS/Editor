28320ead4fe04f83e092899df4aa4b34262a9c91
Julien Moreau-Mathis <ext.julien.moreau-mathis@fdj-gs.com>
2025-12-04 14:32:04 +0100

    fix: set navmesh preload as a plugin to avoid imposing @recast-navigation and @babylonjs/addons packages to be installed

--- tools/src/loading/script/preload.ts
+++ tools/src/loading/script/preload.ts
@@ -5,12 +5,38 @@ import { AdvancedAssetContainer } from "../container";
 
 import { preloadSceneScriptAsset } from "./preload/scene";
 import { preloadCommonScriptAsset } from "./preload/common";
-import { preloadNavMeshScriptAsset } from "./preload/navmesh";
 
 /**
- * Defines the cache of all
+ * Defines the cache of all preloaded assets for scripts.
+ * Used to populate decorated properties in scripts using @visibleAsAsset.
  */
 export const scriptAssetsCache = new Map<string, any>();
+
+/**
+ * Defines the map of all parsers available to parse script assets.
+ * Some assets may not be needed by the game directly such as navmeshes which require @recast-navigation packages to be included.
+ * This map allows to register parsers only when needed to profit from tree shaking.
+ */
+export const scriptAssetsParsers = new Map<string, (parameters: IScriptAssetParserParameters) => Promise<unknown>>();
+
+export interface IScriptAssetParserParameters {
+	key: string;
+	rootUrl: string;
+	scene: Scene;
+}
+
+/**
+ *
+ * @param extension defines the extension supported by the parser. ie: "navmesh"
+ * @param parser defines the parser function to parse the asset.
+ */
+export function registerScriptAssetParser(extension: string, parser: (parameters: IScriptAssetParserParameters) => Promise<unknown>) {
+	if (scriptAssetsParsers.has(extension)) {
+		console.warn(`A parser for the extension '${extension}' was already registered. It will be overwritten.`);
+	}
+
+	scriptAssetsParsers.set(extension, parser);
+}
 
 /**
  * @internal
@@ -56,12 +82,14 @@ export async function _preloadScriptsAssets(rootUrl: string, scene: Scene, scrip
 							scriptAssetsCache.set(key, new AdvancedAssetContainer(await preloadSceneScriptAsset(key, rootUrl, scene), rootUrl, scriptsMap));
 							break;
 
-						case "navmesh":
-							scriptAssetsCache.set(key, await preloadNavMeshScriptAsset(key, rootUrl));
-							break;
-
 						default:
-							scriptAssetsCache.set(key, await preloadCommonScriptAsset(key, rootUrl));
+							if (scriptAssetsParsers.has(extension!)) {
+								const parser = scriptAssetsParsers.get(extension!)!;
+								scriptAssetsCache.set(key, await parser({ key, rootUrl, scene }));
+								break;
+							} else {
+								scriptAssetsCache.set(key, await preloadCommonScriptAsset(key, rootUrl));
+							}
 							break;
 					}
 				} catch (e) {
