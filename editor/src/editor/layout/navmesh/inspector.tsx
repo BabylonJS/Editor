83e34d533567a105ec1960c04c63cda1660dae0b
julien-moreau <moreau-mathis@intechinfo.fr>
2026-02-16 18:14:23 +0100

    fix: don't regenerate navmesh when value change in navmesh editor

--- editor/src/editor/layout/navmesh/inspector.tsx
+++ editor/src/editor/layout/navmesh/inspector.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import { Grid } from "react-loader-spinner";
+
+import { Button } from "../../../ui/shadcn/ui/button";
+
+import { wait } from "../../../tools/tools";
+
 import { Editor } from "../../main";
+
 import { EditorInspectorNumberField } from "../inspector/fields/number";
 import { EditorInspectorSectionField } from "../inspector/fields/section";
 
@@ -12,47 +20,34 @@ export interface INavMeshEditorInspectorProps {
 export function NavMeshEditorInspector(props: INavMeshEditorInspectorProps) {
 	const parameters = props.navMeshEditor.configuration.navMeshParameters;
 
+	const [building, setBuilding] = useState(false);
+
+	async function handleRebuildNavMesh() {
+		setBuilding(true);
+
+		await wait(0);
+		await props.navMeshEditor.updateNavMesh();
+
+		setBuilding(false);
+	}
+
 	return (
 		<div className="flex flex-col gap-2 w-80 h-full p-2">
 			<div className="flex justify-center items-center">Inspector</div>
 
 			<EditorInspectorSectionField title="Parameters">
-				<EditorInspectorNumberField object={parameters} property="cs" label="Cell Size" min={10} step={0.1} onFinishChange={() => props.navMeshEditor.updateNavMesh()} />
-				<EditorInspectorNumberField object={parameters} property="ch" label="Cell Height" min={0.1} step={0.1} onFinishChange={() => props.navMeshEditor.updateNavMesh()} />
+				<EditorInspectorNumberField object={parameters} property="cs" label="Cell Size" min={10} step={0.1} />
+				<EditorInspectorNumberField object={parameters} property="ch" label="Cell Height" min={0.1} step={0.1} />
 
-				<EditorInspectorNumberField
-					object={parameters}
-					property="walkableHeight"
-					label="Walkable Height"
-					min={0.1}
-					step={0.1}
-					onFinishChange={() => props.navMeshEditor.updateNavMesh()}
-				/>
-				<EditorInspectorNumberField
-					object={parameters}
-					property="walkableRadius"
-					label="Walkable Radius"
-					min={0.1}
-					step={0.1}
-					onFinishChange={() => props.navMeshEditor.updateNavMesh()}
-				/>
-				<EditorInspectorNumberField
-					object={parameters}
-					property="walkableSlopeAngle"
-					label="Walkable Slope Angle"
-					min={0.1}
-					max={90}
-					step={0.1}
-					onFinishChange={() => props.navMeshEditor.updateNavMesh()}
-				/>
-				<EditorInspectorNumberField
-					object={parameters}
-					property="walkableClimb"
-					label="Walkable Climb"
-					min={0.1}
-					step={0.1}
-					onFinishChange={() => props.navMeshEditor.updateNavMesh()}
-				/>
+				<EditorInspectorNumberField object={parameters} property="walkableHeight" label="Walkable Height" min={0.1} step={0.1} />
+				<EditorInspectorNumberField object={parameters} property="walkableRadius" label="Walkable Radius" min={0.1} step={0.1} />
+				<EditorInspectorNumberField object={parameters} property="walkableSlopeAngle" label="Walkable Slope Angle" min={0.1} max={90} step={0.1} />
+				<EditorInspectorNumberField object={parameters} property="walkableClimb" label="Walkable Climb" min={0.1} step={0.1} />
+
+				<Button className="flex items-center gap-2" disabled={building} onClick={handleRebuildNavMesh}>
+					{building && <Grid width={16} height={16} color="gray" />}
+					Rebuild NavMesh
+				</Button>
 			</EditorInspectorSectionField>
 		</div>
 	);
