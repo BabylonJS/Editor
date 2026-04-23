f923df1e733ab9efb31ee90d48e4ab5891c81284
julien-moreau <moreau-mathis@intechinfo.fr>
2026-04-23 14:03:01 +0200

    fix: compute connected transform nodes for sounds before scene render

--- editor/src/project/load/plugins/sounds.ts
+++ editor/src/project/load/plugins/sounds.ts
@@ -30,14 +30,14 @@ export async function loadSounds(editor: Editor, soundFiles: string[], scene: Sc
 				sound.id = data.id;
 				sound.uniqueId = data.uniqueId;
 
-				scene.onBeforeRenderObservable.addOnce(() => {
-					if (data.spatialSound && !sound["_connectedTransformNode"]) {
-						const attachedNode = scene.getMeshById(data.connectedMeshId) ?? scene.getTransformNodeById(data.connectedMeshId);
-						if (attachedNode) {
-							sound.attachToMesh(attachedNode);
-						}
+				if (data.spatialSound && !sound["_connectedTransformNode"]) {
+					const attachedNode = scene.getMeshById(data.connectedMeshId) ?? scene.getTransformNodeById(data.connectedMeshId);
+					if (attachedNode) {
+						sound.attachToMesh(attachedNode);
 					}
+				}
 
+				scene.onBeforeRenderObservable.addOnce(() => {
 					// TODO: Find a better way to handle spatial sound property in Babylon.js.
 					// sound.spatialSound is always overridden to true on sound.serialize().
 					sound.spatialSound = data.spatialSound;
