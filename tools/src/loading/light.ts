316d94956dac7208c57cd2fcc8bce860575ee49d
julien-moreau <moreau-mathis@intechinfo.fr>
2026-05-16 15:50:43 +0200

    fix: always create clustered light container in babylonjs-editor-tools

--- tools/src/loading/light.ts
+++ tools/src/loading/light.ts
@@ -2,10 +2,11 @@ import { Scene } from "@babylonjs/core/scene";
 import { ClusteredLightContainer } from "@babylonjs/core/Lights/Clustered/clusteredLightContainer";
 
 export function configureLights(scene: Scene, clusteredLightContainer?: ClusteredLightContainer) {
+	clusteredLightContainer ??= new ClusteredLightContainer("Clustered Light Container", [], scene);
+
 	const clusteredLight = scene.metadata?.clusteredLight;
 	if (clusteredLight) {
 		if (clusteredLight.lights.length > 0) {
-			clusteredLightContainer ??= new ClusteredLightContainer("Clustered Light Container", [], scene);
 			clusteredLightContainer.horizontalTiles = clusteredLight.horizontalTiles;
 			clusteredLightContainer.verticalTiles = clusteredLight.verticalTiles;
 			clusteredLightContainer.depthSlices = clusteredLight.depthSlices;
