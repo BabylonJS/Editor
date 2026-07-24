846bf3623642f37325dde985b4d058865f88fbe4
julien-moreau <moreau-mathis@intechinfo.fr>
2026-07-24 12:44:33 +0200

    feat: add support of gaussian splatting in babylonjs-editor-tools

---
+++ tools/src/loading/gaussian-splatting.ts
@@ -0,0 +1,40 @@
+import { Scene } from "@babylonjs/core/scene";
+import { AssetContainer } from "@babylonjs/core/assetContainer";
+import { AddParser } from "@babylonjs/core/Loading/Plugins/babylonFileParser.function";
+import { GaussianSplattingMesh } from "@babylonjs/core/Meshes/GaussianSplatting/gaussianSplattingMesh";
+
+import { loadFile } from "../tools/request";
+
+let registered = false;
+
+export function registerGaussianSplattingParser() {
+	if (registered) {
+		return;
+	}
+
+	registered = true;
+
+	AddParser("GaussianSplattingMeshEditorPlugin", (parsedData: any, scene: Scene, container: AssetContainer, rootUrl: string) => {
+		parsedData.meshes?.forEach((mesh) => {
+			if (mesh.type !== "GaussianSplattingMesh" || !mesh.splatDataPath) {
+				return;
+			}
+
+			const instantiatedMesh = scene.getMeshById(mesh.id) as GaussianSplattingMesh;
+			if (!instantiatedMesh) {
+				return;
+			}
+
+			const splatDataUrl = rootUrl + mesh.splatDataPath;
+			scene.addPendingData(splatDataUrl);
+
+			loadFile(splatDataUrl, "arraybuffer").then(async (data) => {
+				instantiatedMesh.updateData(data, undefined, {
+					flipY: mesh._flipY,
+				});
+
+				scene.removePendingData(splatDataUrl);
+			});
+		});
+	});
+}
